1710fd5a0d0b2133ea7125ca10dea347f33b18a4
Mahdihrs <mahdiharis@outlook.com>
2018-12-13 21:54:52 +0700

    orm sequelize done

---
+++ models/author.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Author = sequelize.define('Author', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    religion: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    age: DataTypes.INTEGER
+  }, {});
+  Author.associate = function(models) {
+    // associations can be defined here
+    
+  };
+  return Author;
+};
